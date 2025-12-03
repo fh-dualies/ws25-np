16a1ea109799746c193a987425ad9b706a8b40f6
Dennis Höllmann <d.hoellmann@shopware.com>
2025-12-03 10:01:59 +0100

    fix: echo server

--- Praktikum3/tcp_examples/tcp_echo_server_thread.c
+++ Praktikum3/tcp_examples/tcp_echo_server_thread.c
@@ -23,50 +23,37 @@ void handle(int client_fd);
 
 int main(int argc, char **argv)
 {
-    if(argc < 3) {
-        fprintf(stderr, "Usage: %s <server_address> <port>\n", argv[0]);
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         return 1;
     }
 
     int parent_fd, client_fd, opt_val;
-    struct sockaddr *server_addr, client_addr;
-    socklen_t server_addrlen;
-    struct sockaddr_in server_addr_v4;
-    struct sockaddr_in6 server_addr_v6;
+    struct sockaddr client_addr;
+    struct sockaddr_in6 server_addr;
     struct thread_data *thread_data;
     ssize_t len;
 
-    long port = htons(strtol(argv[2], NULL, 10));
-    if (inet_pton(AF_INET, argv[1], &server_addr_v4.sin_addr) != 0) {
-        server_addr = (struct sockaddr *)&server_addr_v4;
-        server_addrlen = sizeof(struct sockaddr_in);
-        server_addr_v4.sin_family = AF_INET;
-        #ifdef HAVE_SIN_LEN
-        server_addr_v4.sin_len = sizeof(struct sockaddr_in);
-        #endif
-        server_addr_v4.sin_port = port;
-    } else if (inet_pton(AF_INET6, argv[1], &server_addr_v6.sin6_addr) != 0) {
-        server_addr = (struct sockaddr *)&server_addr_v6;
-        server_addrlen = sizeof(struct sockaddr_in6);
-        server_addr_v6.sin6_family = AF_INET6;
-        #ifdef HAVE_SIN_LEN
-        server_addr_v6.sin6_len = sizeof(struct sockaddr_in6);
-        #endif
-        server_addr_v6.sin6_port = port;
-    } else {
-        fprintf(stderr, "No valid adress found in: %s\n", argv[1]);
-        return 1;
-    }
+    long port = htons(strtol(argv[1], NULL, 10));
 
-    parent_fd = Socket(server_addr->sa_family, SOCK_STREAM, IPPROTO_TCP);
+    server_addr.sin6_family = AF_INET6;
+    server_addr.sin6_port = port;
+    server_addr.sin6_addr = in6addr_loopback;
+    #ifdef HAVE_SIN_LEN
+    server_addr.sin6_len = sizeof(struct sockaddr_in6);
+    #endif
+
+    parent_fd = Socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
     opt_val = 1;
     SetSockOpt(parent_fd, SOL_SOCKET, SO_REUSEADDR, (const void *) &opt_val, sizeof(int), true);
+    int v6only = 0;
+    SetSockOpt(parent_fd, IPPROTO_IPV6, IPV6_V6ONLY, &v6only, sizeof(int), false);
 
-    Bind(parent_fd, server_addr, server_addrlen, true);
+    Bind(parent_fd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr_in6), true);
     Listen(parent_fd, 10);
 
     for (;;) {
-        client_fd = Accept(parent_fd, (struct sockaddr *) &client_addr, (socklen_t *)&len);
+        client_fd = Accept(parent_fd, NULL, NULL);
 
         thread_data = (struct thread_data *) malloc(sizeof(struct thread_data));
         thread_data->client_fd = client_fd;
