b8c27f7a437ec0976b8a58dbf2248fe5308d50e9
Dennis Höllmann <dennis.hoellmann@protonmail.com>
2025-12-08 20:33:11 +0100

    feat: implement error messages

--- client/src/client.c
+++ client/src/client.c
@@ -9,12 +9,55 @@
 
 #include "cblib.h"
 
+#define MESSAGE_COLUMN_TYPE 1
+#define MESSAGE_COLUMN_ACK_TYPE 2
+#define MESSAGE_HEARTBEAT_TYPE 3
+#define MESSAGE_HEARTBEAT_ACK_TYPE 4
+#define MESSAGE_ERROR_TYPE 5
+
+#define ERROR_CODE_UNKNOWN_TYPE 1
+#define ERROR_CODE_PROTOCOL_ERROR 2
+
 struct State {
     int socket_fd;
+} state = {-1};
+
+char buffer[1<<16];
+
+struct MessageUnknown {
+    uint16_t type;
+    uint16_t length;
+    uint8_t data[];
 };
 
-int create_udp_socket(uint16_t port) {
-    int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+struct MessageColumn {
+    uint16_t type;
+    uint16_t length;
+    uint32_t sequence;
+    uint16_t column;
+};
+
+struct MessageColumnAck {
+    uint16_t type;
+    uint16_t length;
+    uint32_t ack;
+};
+
+struct MessageHeartbeat {
+    uint16_t type;
+    uint16_t length;
+    uint8_t data[];
+};
+
+struct MessageError {
+    uint16_t type;
+    uint16_t length;
+    uint32_t error_code;
+    uint8_t data[];
+};
+
+int create_udp_socket(const uint16_t port) {
+    const int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (socket_fd == -1) {
         perror("socket");
         return -1;
@@ -37,7 +80,7 @@ int create_udp_socket(uint16_t port) {
     return socket_fd;
 }
 
-int connect_peer(int socket_fd, uint32_t peer_ip, uint16_t peer_port) {
+int connect_peer(const uint32_t peer_ip, const uint16_t peer_port) {
     struct sockaddr_in address;
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
@@ -46,7 +89,7 @@ int connect_peer(int socket_fd, uint32_t peer_ip, uint16_t peer_port) {
 #ifdef HAVE_SIN_LEN
     server_addr.sin_len = sizeof(struct sockaddr_in);
 #endif
-    if (connect(socket_fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
+    if (connect(state.socket_fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
         perror("connect");
         return -1;
     }
@@ -54,45 +97,108 @@ int connect_peer(int socket_fd, uint32_t peer_ip, uint16_t peer_port) {
 }
 
 void on_stdin(void* arg) {
-    struct State *state = arg;
-    char buffer[1<<16];
-    ssize_t r = read(STDIN_FILENO, buffer, sizeof(buffer));
+    const ssize_t r = read(STDIN_FILENO, buffer, sizeof(buffer));
     if (r == -1) {
         perror("read");
     } else {
-        ssize_t s_count = send(state->socket_fd, buffer, r, 0);
+        const ssize_t s_count = send(state.socket_fd, buffer, r, 0);
         printf("SEND %ld BYTES\n", s_count);
     }
 }
 
-void on_message(void* arg) {
-    struct State *state = arg;
-    char buffer[1<<16];
-    ssize_t msg_size = read(state->socket_fd, buffer, sizeof(buffer));
-    if (msg_size == -1) {
-        perror("recv");
+void send_to_peer(const struct MessageUnknown* data, const size_t total_size) {
+    // Ensures padding at the end. Padding contents will be random data from the buffer.
+    const size_t padded_size = total_size + (4 - (total_size % 4));
+    memcpy(buffer, data, total_size);
+    if (send(state.socket_fd, buffer, padded_size, 0) == -1) {
+        perror("send");
     }
-    write(STDOUT_FILENO, buffer, msg_size);
 }
 
-void start_client(uint16_t own_port, uint32_t peer_ip, uint16_t peer_port) {
-    printf("Start client on Port %d. With Peer %u:%d\n", own_port, peer_ip, peer_port);
-    int socket_fd = create_udp_socket(own_port);
-    if (socket_fd == -1) return;
+void send_error_message(const uint32_t error_code, const char* error_string) {
+    const size_t err_string_size = strlen(error_string);
+    const size_t message_size = sizeof(struct MessageError) + err_string_size;
+    struct MessageError *msg = malloc(message_size);
+    msg->type = htons(MESSAGE_ERROR_TYPE);
+    msg->length = htons(err_string_size + 4); // +4 to include the error code;
+    msg->error_code = htonl(error_code);
+    memcpy(msg->data, error_string, err_string_size);
+    send_to_peer((struct MessageUnknown*) msg, message_size);
+    free(msg);
 
-    if (connect_peer(socket_fd, peer_ip, peer_port) == -1) {
-        close(socket_fd);
+#ifdef DEBUG
+    fprintf(stderr, "Send error message with code %d to Peer: %s", error_code, error_string);
+#endif
+}
+
+void recv_error_message(struct MessageUnknown* msg) {
+    if (msg->length < 4) return; // Error code is 4 byte
+    struct MessageError *error = (struct MessageError*)msg;
+    error->error_code = ntohs(error->error_code);
+    const size_t err_string_size = msg->length - 4;
+    printf("Recieved error message with code %d: %.*s\n", error->error_code, (int)err_string_size, (char *)error->data);
+}
+
+void on_message(void* arg) {
+    const ssize_t msg_size = recv(state.socket_fd, buffer, sizeof(buffer), 0);
+    if (msg_size == -1) {
+        perror("recv");
+        return;
+    }
+
+    if (msg_size < sizeof(struct MessageUnknown)) {
+#ifdef DEBUG
+        fprintf(stderr, "Message too short\n");
+#endif
+        return;
+    }
+
+    struct MessageUnknown *message = (struct MessageUnknown*)buffer;
+    message->length = ntohs(message->length);
+    message->type = ntohs(message->type);
+
+    if (msg_size < (sizeof(struct MessageUnknown) + message->length)) {
+#ifdef DEBUG
+        fprintf(stderr, "Message length does not match size\n");
+#endif
+        return;
+    }
+
+    switch (message->type) {
+        case MESSAGE_COLUMN_TYPE:
+            puts("Column type");
+            break;
+        case MESSAGE_COLUMN_ACK_TYPE:
+            puts("Column ack");
+            break;
+        case MESSAGE_HEARTBEAT_TYPE:
+            puts("Heartbeat");
+            break;
+        case MESSAGE_HEARTBEAT_ACK_TYPE:
+            puts("Heartbeat ack");
+            break;
+        case MESSAGE_ERROR_TYPE:
+            recv_error_message(message);
+            break;
+        default:
+            send_error_message(ERROR_CODE_UNKNOWN_TYPE, "Message type unknown");
+            break;
+    }
+}
+
+void start_client(const uint16_t own_port, const uint32_t peer_ip, const uint16_t peer_port) {
+    state.socket_fd = create_udp_socket(own_port);
+    if (state.socket_fd == -1) return;
+
+    if (connect_peer(peer_ip, peer_port) == -1) {
+        close(state.socket_fd);
         return;
     }
 
     init_cblib();
 
-    struct State state = {
-        socket_fd,
-    };
-
-    register_stdin_callback(on_stdin, &state);
-    register_fd_callback(socket_fd, on_message, &state);
+    register_stdin_callback(on_stdin, NULL);
+    register_fd_callback(state.socket_fd, on_message, NULL);
 
     handle_events();
 }
